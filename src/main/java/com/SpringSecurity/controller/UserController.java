0cf1a38f9978f1a6a68e3093e160c3019b0d5756
gaurabpokharel <gaurabpokharel3@gmail.com>
2021-01-18 19:42:11 +0545

    cors

--- src/main/java/com/SpringSecurity/controller/UserController.java
+++ src/main/java/com/SpringSecurity/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ import com.SpringSecurity.security.SecurityService;
 import com.SpringSecurity.service.UserService;
 
 @RestController
-
+@CrossOrigin(origins = "*")
 public class UserController {
 	
 	@Autowired
